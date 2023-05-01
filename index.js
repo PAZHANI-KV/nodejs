e6c0dfab585ecbd056e3ac353d2f74c20b6c8c85
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-05-01 14:36:02 +0530

    change

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
- //This file have the contents about a task in node!
+//This file have the contents about a task in node!
 
- import { timeStamp } from "console";
+import { timeStamp } from "console";
 import express from "express";
- import * as fs from 'fs'; //file accessing packages
+import * as fs from 'fs'; //file accessing packages
 
- //initialising express server
+//initialising express server
 
- const app = express();
- let time = new Date();
- let datestring = time.toUTCString().slice(0, -4);
- console.log(datestring)
+const app = express();
+let time = new Date();
+let datestring = time.toUTCString().slice(0, -4);
+console.log(datestring)
 
-//  const timestamp = `Last created timestamp: ${datestring}`;
+const timestamp = `Last created timestamp: ${datestring}`;
 
-//  fs.writeFile("/", timeStamp)
+// Lines 18, 19, 20 are the setup to write a file!
+
+// fs.writeFile("/", timeStamp, () => {
+//    console.log("file created!")
+// })
 
 
 //APIs
- app.get("/", (req, res) => {
-    res.send("Hey there,Im working fine!")
- })
+app.get("/", (req, res) => {
+   res.send("Hey there,Im working fine!")
+})
 
 
- //set server to listen under port 9000!
+//set server to listen under port 9000!
 
- app.listen(9000, () => console.log("server started in localhost 9000"))
+app.listen(9000, () => console.log("server started in localhost 9000"))
